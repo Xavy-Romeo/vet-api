ce105639bf31ff293b6014fcc1f0e96cb871e8d6
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2022-07-26 18:28:10 -0700

    Spring Boot initial commit

---
+++ src/main/java/com/personal/vetapi/AnimalHospitalApiApplication.java
@@ -0,0 +1,13 @@
+package com.personal.vetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalHospitalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalHospitalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/vetapi/AnimalHospitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.vetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalHospitalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
